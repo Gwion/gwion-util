567be9d3c79e357aea39292ad801ff97f7a39c23
fennecdjay <astor.jeremie@wanadoo.fr>
2018-11-16 12:37:16 +0100

    :bug: Fix header

--- include/gwion_util.h
+++ include/gwion_util.h
@@ -1,16 +1,12 @@
 #ifndef __GWION_UTIL
 #define __GWION_UTIL
-#include "absyn.h"
 #include "defs.h"
-#include "err_msg.h"
-#include "generated.h"
-#include "hash.h"
-#include "lexer.h"
 #include "map.h"
+#include "hash.h"
+#include "absyn.h"
+#include "scanner.h"
+#include "err_msg.h"
 #include "map_private.h"
 #include "mpool.h"
-#include "mpool_private.h"
-#include "parser.h"
-#include "scanner.h"
 #include "symbol.h"
 #endif
