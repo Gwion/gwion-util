640e309e884b5929b4c06936802421b2dd1272f8
fennecdjay <astor.jeremie@wanadoo.fr>
2018-11-16 12:25:38 +0100

    :art: Add global header

---
+++ include/gwion_util.h
@@ -0,0 +1,16 @@
+#ifndef __GWION_UTIL
+#define __GWION_UTIL
+#include "absyn.h"
+#include "defs.h"
+#include "err_msg.h"
+#include "generated.h"
+#include "hash.h"
+#include "lexer.h"
+#include "map.h"
+#include "map_private.h"
+#include "mpool.h"
+#include "mpool_private.h"
+#include "parser.h"
+#include "scanner.h"
+#include "symbol.h"
+#endif
