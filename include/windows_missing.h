3527e5acf54d92754941faa932048cee7cd159a5
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-14 14:21:55 +0100

    :art: scandir

--- include/windows_missing.h
+++ include/windows_missing.h
@@ -8,7 +8,4 @@ char* strsep(char** stringp, const char* delim);
 int getline(char **lineptr, size_t *n, FILE *fp);
 FILE *fmemopen(void *buf, size_t len, const char *type __attribute__((unused)));
 char *realpath(const char *path, char *resolved_path);
-int scandir(const char *dirname, struct dirent ***namelist,
-  int (*select)(struct dirent *),
-  int (*dcomp)(const void *, const void *));
 #endif
