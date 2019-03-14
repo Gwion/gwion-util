767b7a726d443700dda4d52fce58924b4294a55c
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-14 13:18:20 +0100

    :art: Update header

--- include/windows_missing.h
+++ include/windows_missing.h
@@ -6,5 +6,5 @@ char* strndup(const char *s, size_t n);
 char* strsep(char** stringp, const char* delim);
 int getline(char **lineptr, size_t *n, FILE *fp);
 FILE *fmemopen(void *buf, size_t len, const char *type __attribute__((unused)));
-char *realpath(const char *path, char resolved_path[PATH_MAX]);
+char *realpath(const char *path, char *resolved_path);
 #endif
