4a8936442d64c620fde6a917da012f5a7f9857d0
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-14 12:45:47 +0100

    :art: strdup

--- include/windows_missing.h
+++ include/windows_missing.h
@@ -1,6 +1,7 @@
 #ifndef __WINDOWS_MISSING
 #include <stddef.h>
 typedef unsigned int uint;
+char *strdup(const char *s);
 char *strndup(const char *s, size_t n);
-//int getline(char **lineptr, size_t *n, FILE *fp);
+int getline(char **lineptr, size_t *n, FILE *fp);
 #endif
--- windows_missing/strndup.c
+++ windows_missing/strndup.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *strdup(const char *s) {
+    char *p = malloc(strlen(s) + 1);
+    if(p)
+      strcpy(p, s);
+    return p;
+}
+
 char *strndup(const char *s, size_t n) {
   char* c = malloc(n+1);
   if(c) {
