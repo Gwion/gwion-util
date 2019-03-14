d9fcdca2764901294181f2237bd6e37cf557b149
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-14 12:13:25 +0100

    :art: fmemopen

--- include/windows_missing.h
+++ include/windows_missing.h
@@ -3,4 +3,5 @@
 typedef unsigned int uint;
 char *strndup(const char *s, size_t n);
 int getline(char **lineptr, size_t *n, FILE *fp);
+FILE *fmemopen(void *buf, size_t size, const char *type);
 #endif
