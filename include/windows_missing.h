46a20a9c4b102fab885d38e56a8d3b731a3e0716
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-14 12:25:28 +0100

    :art: Makefile

--- include/windows_missing.h
+++ include/windows_missing.h
@@ -2,6 +2,5 @@
 #include <stddef.h>
 typedef unsigned int uint;
 char *strndup(const char *s, size_t n);
-int getline(char **lineptr, size_t *n, FILE *fp);
-FILE *fmemopen(void *buf, size_t size, const char *type);
+//int getline(char **lineptr, size_t *n, FILE *fp);
 #endif
