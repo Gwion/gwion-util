31a2b1b528468d165771bf00495ef3d3bede49ea
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-18 13:56:40 +0100

    :art: Fix macro

--- include/windows_missing.h
+++ include/windows_missing.h
@@ -13,7 +13,8 @@ char *realpath(const char *path, char *resolved_path);
 #define MUTEX_TYPE             HANDLE
 #define MUTEX_INITIALIZER      NULL
 #define MUTEX_SETUP(x)         (x) = CreateMutex(NULL, FALSE, NULL)
-#define MUTEX_CLEANUP(x)       (CloseHandle(x) == 0)
+//#define MUTEX_CLEANUP(x)       (CloseHandle(x) == 0)
+#define MUTEX_CLEANUP(x)       CloseHandle(x)
 #define MUTEX_LOCK(x)          emulate_pthread_mutex_lock((x))
 //#define MUTEX_UNLOCK(x)        (ReleaseMutex(x) == 0)
 #define MUTEX_UNLOCK(x)        ReleaseMutex((x))
