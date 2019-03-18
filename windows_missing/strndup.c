08096b025b1f4a2860381601b4e945082afa2059
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-18 14:33:11 +0100

    :art: new strsep implementation

--- windows_missing/strndup.c
+++ windows_missing/strndup.c
@@ -17,16 +17,22 @@ char *strndup(const char *s, size_t n) {
   return c;
 }
 
-char* strsep(char** stringp, const char* delim) {
-  char* start = *stringp;
-  char* p;
+char* strsep(char **stringp, const char *delim) {
+	char *start = *stringp;
+	char *ptr;
 
-  if(!(p = (!start) ? strpbrk(start, delim) : NULL))
-    *stringp = NULL;
-  else {
-    *p = '\0';
-    *stringp = p + 1;
-  }
-
-  return start;
+	if(!start)
+		return NULL;
+	if(!*delim)
+		ptr = start + strlen(start);
+	else {
+		ptr = strpbrk(start, delim);
+		if(!ptr) {
+			*stringp = NULL;
+			return start;
+		}
+	}
+	*ptr = '\0';
+	*stringp = ptr + 1;
+	return start;
 }
