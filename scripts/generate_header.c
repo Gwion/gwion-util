b522c625d23acb83ba35d393d5c0cec2ce74056f
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-18 13:35:39 +0100

    :art: Fix format

--- scripts/generate_header.c
+++ scripts/generate_header.c
@@ -7,9 +7,9 @@
 #ifdef BUILD_ON_WINDOWS
 #include "windows_missing.h"
 #if _WIN64
-#define SIZET_FMT "%llu"
+#define SIZET_FMT "%I64u"
 #else
-#define SIZET_FMT "%lu"
+#define SIZET_FMT "%I32u"
 #endif
 #else
 #define SIZET_FMT "%zu"
