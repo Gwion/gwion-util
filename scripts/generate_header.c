62bb00537eada425e9d748433f2b1745d252afb1
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-18 14:24:06 +0100

    :wrench: Add other mingw macro

--- scripts/generate_header.c
+++ scripts/generate_header.c
@@ -6,14 +6,17 @@
 
 #ifdef BUILD_ON_WINDOWS
 #include "windows_missing.h"
+#define __USE_MINGW_ANSI_STDIO 1
+/*
 #ifdef __MINGW32__
 #define SIZET_FMT "%I32u"
 #else
 #define SIZET_FMT "%I64u"
 #endif
 #else
-#define SIZET_FMT "%zu"
+*/
 #endif
+#define SIZET_FMT "%zu"
 
 #ifdef USE_DOUBLE
 static const char* type	 = "double";
