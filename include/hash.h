ca80e9e39bd8de3415f6b0078f06e5e0b34c8c99
fennecdjay <astor.jeremie@wanadoo.fr>
2019-03-29 00:54:49 +0100

    :art: Update hash

--- include/hash.h
+++ include/hash.h
@@ -9,5 +9,21 @@ struct Hash_ {
 void  hini(const Hash, const uint);
 void  hdel(const Hash, void (*)(void*));
 void  hend(const Hash);
-ANN unsigned int hash(const char *s0);
+ANN unsigned int hash(const m_str s0);
+
+#define haction(type, ret_type, argtype, prefix, func, pre, cmp, action, post, ret) \
+ret_type prefix##_##func(const Hash h, argtype arg)  { \
+  const unsigned int idx = hash((m_str)arg) % h->size;        \
+  const type sym = h->table[idx];                      \
+  pre                                                  \
+  for(type s = sym; s; s = s->next) {                  \
+    if(cmp) { action }                                 \
+    post                                               \
+  }                                                    \
+  ret;                                                 \
+}
+
+// should go to ast
+#define hstraction(type, ret_type, func, pre, action, post, ret) \
+haction(type, ret_type, m_str, macro, func, pre, !strcmp(s->name, arg), action, post, ret)
 #endif
