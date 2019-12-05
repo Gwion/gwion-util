1232e82528b863d13c604aa3a88538641a7731e4
Jérémie Astor <astor.jeremie@wanadoo.fr>
2019-12-05 20:30:04 +0100

    :art: Update

--- include/gwion_text.h
+++ include/gwion_text.h
@@ -19,7 +19,7 @@ ANN static inline void text_release(GwText *text) {
 // TODO: renaming
 ANN static inline void text_reset(GwText *text) {
   if(text->str) {
-    text->str = '\0';
+    *text->str = '\0';
     text->cap = text->len = 0;
   }
 }
