6e4589f86c3d1021e193d8985bff3cc7379f8e44
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-04-01 18:55:45 +0200

    :art: Improve gwion_thread

--- include/gwion_thread.h
+++ include/gwion_thread.h
@@ -2,18 +2,19 @@
 #define __GWION_THREAD
 #ifdef BUILD_ON_WINDOWS
 #include <windows.h>
-#define THREAD_FUNC(a)            long unsigned int (a)(void *data)
+#define THREAD_RETTYPE            long unsigned int
+#define THREAD_FUNC(a)            THREAD_RETTYPE (a)(void *data)
 #define THREAD_TYPE               HANDLE
 #define THREAD_CREATE(thread, func, arg)  thread = CreateThread(NULL, 0, func, arg, 0, NULL);
 #define THREAD_JOIN(thread)   WaitForSingleObject(thread, 0);
-#define THREAD_RETURN(arg)        return arg;
+#define THREAD_RETURN(arg)        return (THREAD_RETTYPE)arg;
 
 #define MUTEX_TYPE             HANDLE
 #define MUTEX_INITIALIZER      NULL
 #define MUTEX_SETUP(x)         (x) = CreateMutex(NULL, FALSE, NULL)
 #define MUTEX_CLEANUP(x)       CloseHandle(x)
 #define MUTEX_LOCK(x)          emulate_pthread_mutex_lock(&(x))
-#define MUTEX_UNLOCK(x)        ReleaseMutex((x))
+#define MUTEX_UNLOCK(x)        (ReleaseMutex((x)) == 0)
 int emulate_pthread_mutex_lock(volatile MUTEX_TYPE *mx);
 
 #define THREAD_COND_TYPE           HANDLE
@@ -24,11 +25,12 @@ int emulate_pthread_mutex_lock(volatile MUTEX_TYPE *mx);
 #else
 #include <pthread.h>
 
-#define THREAD_FUNC(a)               void* (a)(void *data)
+#define THREAD_RETTYPE            void*
+#define THREAD_FUNC(a)            THREAD_RETTYPE (a)(void *data)
 #define THREAD_TYPE               pthread_t
 #define THREAD_CREATE(thread, func, arg)  pthread_create(&thread, NULL, func, arg);
 #define THREAD_JOIN(thread)       pthread_join(thread, NULL);
-#define THREAD_RETURN(arg)        pthread_exit(arg); return arg;
+#define THREAD_RETURN(arg)        { pthread_exit((THREAD_RETTYPE)arg); return arg; }
 
 #define MUTEX_TYPE             pthread_mutex_t*
 #define MUTEX_INITIALIZER      PTHREAD_MUTEX_INITIALIZER
