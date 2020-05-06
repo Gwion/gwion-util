7594f8490040c0ef33a17f893b47fac8e8e5aad0
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-05-06 17:18:22 +0200

    :art: [Windows] use xmalloc

--- windows_missing/strndup.c
+++ windows_missing/strndup.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <string.h>
+#include <defs.h>
 
 char *strdup(const char *s) {
-    char *p = malloc(strlen(s) + 1);
+    char *p = xmalloc(strlen(s) + 1);
     if(p)
       strcpy(p, s);
     return p;
 }
 
 char *strndup(const char *s, size_t n) {
-  char* c = malloc(n+1);
+  char* c = xmalloc(n+1);
   if(c) {
     strncpy(c, s, n);
     c[n] = '\0';
