23a866824c52ee4b86c6668a701e89c23dd83eae
Jérémie Astor <fennecdjay@gmail.com>
2021-05-17 01:07:29 +0200

    :wrench: use -O2

--- common.mk
+++ common.mk
@@ -3,29 +3,29 @@ CFLAGS +=-DUSE_DOUBLE
 endif
 
 ifeq (${USE_DEBUG}, 1)
-CFLAGS += -fsanitize=address -fno-omit-frame-pointer -Og
+CFLAGS  += -fsanitize=address -fno-omit-frame-pointer -Og
 LDFLAGS += -fsanitize=address -fno-omit-frame-pointer -Og
 else
-CFLAGS += -DNDEBUG -fomit-frame-pointer -fno-stack-protector -fno-common -Ofast
-LDFLAGS += -fomit-frame-pointer -fno-stack-protector -fno-common -Ofast
+CFLAGS  += -DNDEBUG -fomit-frame-pointer -fno-stack-protector -fno-common -O2 -fno-math-errno
+LDFLAGS += -fomit-frame-pointer -fno-stack-protector -fno-common -O2 -fno-math-errno
 endif
 
 ifeq (${USE_LTO}, 1)
-CFLAGS += -flto
+CFLAGS  += -flto
 LDFLAGS += -flto
 endif
 
 ifeq (${USE_GETTEXT}, 1)
-CFLAGS += -DUSE_GETTEXT
+CFLAGS  += -DUSE_GETTEXT
 endif
 
 ifeq (${USE_COVERAGE}, 1)
-CFLAGS += -ftest-coverage -fprofile-arcs -O0 -g
+CFLAGS  += -ftest-coverage -fprofile-arcs -O0 -g
 LDFLAGS += --coverage
 endif
 
 ifeq (${BUILD_ON_WINDOWS}, 1)
-CFLAGS += -DBUILD_ON_WINDOWS=1 -D_XOPEN_SOURCE=700 -Wl,--export-all-symbols
+CFLAGS  += -DBUILD_ON_WINDOWS=1 -D_XOPEN_SOURCE=700 -Wl,--export-all-symbols
 LDFLAGS += -Wl,--enable-auto-import -Wl,--export-all-symbols -static -Wl,--out-implib=lib${GWION_PACKAGE}.dll.a
 else
 LDFLAGS += -rdynamic
