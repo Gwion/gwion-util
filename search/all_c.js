19c98a5dafc11bb74397f32abed6cff4abc57dfb
fennecdjay <fennecdjay@gmail.com>
2022-01-08 16:56:25 +0000

    Deploying to gh-pages from  @ 9b1b16a7f3c8583afaa7d68e1395fc70315e4f0f 🚀

--- search/all_c.js
+++ search/all_c.js
@@ -1,17 +1,16 @@
 var searchData=
 [
-  ['n_0',['N',['../mpool_8c.html#a0240ac851181b84ac374872dc5434ee4',1,'mpool.c']]],
-  ['name_1',['name',['../structSymbol__.html#a151a0f094ab23024dbe1b604a346d5de',1,'Symbol_']]],
-  ['nblk_2',['nblk',['../structpool.html#a5f9eb90fb91008ac26901153cea88fa3',1,'pool']]],
-  ['new_5fm_5fvector_3',['new_m_vector',['../m__vector_8h.html#a5601ee490dc2c9a8915af7e77ce94f9b',1,'m_vector.h']]],
-  ['new_5fmap_4',['new_map',['../map_8c.html#abe97c1824252557041888c27bcbe9114',1,'new_map(MemPool p):&#160;map.c'],['../map_8h.html#a549d43a5cd3997b5ec09aba0af098084',1,'new_map(MemPool):&#160;map.c']]],
-  ['new_5fpool_5',['new_pool',['../mpool_8c.html#aac5b0b6d2bc686180a4b55ab8a199902',1,'new_pool(const uint32_t obj_sz):&#160;mpool.c'],['../mpool_8h.html#aa92b21ba8007f24ab32157dd145e0635',1,'new_pool(const uint32_t elementSize):&#160;mpool.c']]],
-  ['new_5fscope_6',['new_scope',['../scope_8h.html#a3aa70fd16a35b7e61ce6850da5f1514e',1,'new_scope(MemPool):&#160;scope.c'],['../scope_8c.html#a13f4d2918a05fba6ee8bd2ae691a612f',1,'new_scope(MemPool p):&#160;scope.c']]],
-  ['new_5fsymbol_5ftable_7',['new_symbol_table',['../symbol_8h.html#acd90a8f5add952795185e36201b424f5',1,'new_symbol_table(MemPool p, const size_t sz):&#160;symbol.c'],['../symbol_8c.html#acd90a8f5add952795185e36201b424f5',1,'new_symbol_table(MemPool p, const size_t sz):&#160;symbol.c']]],
-  ['new_5ftext_8',['new_text',['../gwion__text_8h.html#ae3c46dfa097bba993445a9a68fe121da',1,'gwion_text.h']]],
-  ['new_5fvector_9',['new_vector',['../vector_8h.html#a89885e23100989c05ce45aff61e027cf',1,'new_vector(MemPool p):&#160;vector.c'],['../vector_8c.html#a3b05a62b48975cf61ed8d24b045cf13b',1,'new_vector(MemPool p):&#160;vector.c']]],
-  ['next_10',['next',['../structpool.html#add1ad091b63301b68f59855fe03ec8a2',1,'pool::next()'],['../structRecycle.html#a6030ec7f917c5b6769b7b329fe1d01da',1,'Recycle::next()'],['../structSymbol__.html#a9040c623669a278243e9a8ed757b7e72',1,'Symbol_::next()']]],
-  ['not_11',['not',['../License_8txt.html#a842224abbb72b50c9703a22030a36ee4',1,'License.txt']]],
-  ['num_5fdigit_12',['num_digit',['../defs_8h.html#a5dfe5f2b89d78866fa3aea7c5e3c4e29',1,'defs.h']]],
-  ['nused_13',['NUSED',['../defs_8h.html#a779a45537ce96ee93fd887eef961a634',1,'defs.h']]]
+  ['name_0',['name',['../structSymbol__.html#a151a0f094ab23024dbe1b604a346d5de',1,'Symbol_']]],
+  ['nblk_1',['nblk',['../structpool.html#a5f9eb90fb91008ac26901153cea88fa3',1,'pool']]],
+  ['new_5fm_5fvector_2',['new_m_vector',['../m__vector_8h.html#a5601ee490dc2c9a8915af7e77ce94f9b',1,'m_vector.h']]],
+  ['new_5fmap_3',['new_map',['../map_8h.html#a549d43a5cd3997b5ec09aba0af098084',1,'new_map(MemPool):&#160;map.c'],['../map_8c.html#abe97c1824252557041888c27bcbe9114',1,'new_map(MemPool p):&#160;map.c']]],
+  ['new_5fpool_4',['new_pool',['../mpool_8c.html#aac5b0b6d2bc686180a4b55ab8a199902',1,'new_pool(const uint32_t obj_sz):&#160;mpool.c'],['../mpool_8h.html#aa92b21ba8007f24ab32157dd145e0635',1,'new_pool(const uint32_t elementSize):&#160;mpool.c']]],
+  ['new_5fscope_5',['new_scope',['../scope_8h.html#a3aa70fd16a35b7e61ce6850da5f1514e',1,'new_scope(MemPool):&#160;scope.c'],['../scope_8c.html#a13f4d2918a05fba6ee8bd2ae691a612f',1,'new_scope(MemPool p):&#160;scope.c']]],
+  ['new_5fsymbol_5ftable_6',['new_symbol_table',['../symbol_8h.html#acd90a8f5add952795185e36201b424f5',1,'new_symbol_table(MemPool p, const size_t sz):&#160;symbol.c'],['../symbol_8c.html#acd90a8f5add952795185e36201b424f5',1,'new_symbol_table(MemPool p, const size_t sz):&#160;symbol.c']]],
+  ['new_5ftext_7',['new_text',['../gwion__text_8h.html#ae3c46dfa097bba993445a9a68fe121da',1,'gwion_text.h']]],
+  ['new_5fvector_8',['new_vector',['../vector_8h.html#a89885e23100989c05ce45aff61e027cf',1,'new_vector(MemPool p):&#160;vector.c'],['../vector_8c.html#a3b05a62b48975cf61ed8d24b045cf13b',1,'new_vector(MemPool p):&#160;vector.c']]],
+  ['next_9',['next',['../structpool.html#add1ad091b63301b68f59855fe03ec8a2',1,'pool::next()'],['../structRecycle.html#a6030ec7f917c5b6769b7b329fe1d01da',1,'Recycle::next()'],['../structSymbol__.html#a9040c623669a278243e9a8ed757b7e72',1,'Symbol_::next()']]],
+  ['not_10',['not',['../License_8txt.html#a842224abbb72b50c9703a22030a36ee4',1,'License.txt']]],
+  ['num_5fdigit_11',['num_digit',['../defs_8h.html#a5dfe5f2b89d78866fa3aea7c5e3c4e29',1,'defs.h']]],
+  ['nused_12',['NUSED',['../defs_8h.html#a779a45537ce96ee93fd887eef961a634',1,'defs.h']]]
 ];
--- search/defines_9.js
+++ search/defines_9.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['n_0',['N',['../mpool_8c.html#a0240ac851181b84ac374872dc5434ee4',1,'mpool.c']]],
-  ['nused_1',['NUSED',['../defs_8h.html#a779a45537ce96ee93fd887eef961a634',1,'defs.h']]]
+  ['nused_0',['NUSED',['../defs_8h.html#a779a45537ce96ee93fd887eef961a634',1,'defs.h']]]
 ];
