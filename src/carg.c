6e4abd61c6dcb2f77f2225d3a2082ae6127304aa
fennecdjay <fennecdjay@gmail.com>
2022-09-13 13:24:26 +0200

    :art: Remove VLA in util

--- src/carg.c
+++ src/carg.c
@@ -19,9 +19,10 @@ struct ArgSplitter {
   MemPool mp;
 };
 
-ANN static void _split_args(struct ArgSplitter *as) {
+ANN static void _split_args(MemPool mp, struct ArgSplitter *as) {
   const size_t sz = strlen(as->str);
-  char         buf[sz + 1], prev = '\0';
+  char *buf = mp_malloc2(mp, sz + 1);
+  char prev = '\0';
   m_uint       i = 0, j = 0;
   char         c;
   while ((c = as->str[i]) != '\0') {
@@ -36,13 +37,14 @@ ANN static void _split_args(struct ArgSplitter *as) {
   }
   buf[i] = '\0';
   vector_add(as->v, (vtype)mstrdup(as->mp, buf));
+  mp_free2(mp, sz + 1, buf);
   if (i == sz) return;
   as->str += i + 1;
-  _split_args(as);
+  _split_args(mp, as);
 }
 
 ANN Vector split_args(MemPool p, const m_str str) {
   struct ArgSplitter as = {.str = str, .v = new_vector(p), .mp = p};
-  _split_args(&as);
+  _split_args(p, &as);
   return as.v;
 }
