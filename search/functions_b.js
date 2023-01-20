45a278073537f10e245495d02e9ff7989a196257
fennecdjay <fennecdjay@gmail.com>
2023-01-20 14:38:40 +0000

    Deploying to gh-pages from  @ d4b4d45ac7f64e3a42076a0aca35ab29b57817e4 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -9,8 +9,9 @@ var searchData=
   ['tcol_5fsnprintf_6',['tcol_snprintf',['../termcolor_8c.html#a5d7c664db8584c4396bd5bb6c46939b7',1,'tcol_snprintf(char *stream, size_t N, const char *fmt,...):&#160;termcolor.c'],['../termcolor_8h.html#a04f7ab41736b98fbba6b28c76f0d36f6',1,'tcol_snprintf(char *buffer, size_t N, const char *fmt,...):&#160;termcolor.c']]],
   ['tcol_5fvfprintf_7',['tcol_vfprintf',['../termcolor_8c.html#abceb3cf9bcb5a4958f611458b711e996',1,'termcolor.c']]],
   ['tcol_5fvsnprintf_8',['tcol_vsnprintf',['../termcolor_8c.html#a3328b083df29d219389ca7c1cffb7289',1,'termcolor.c']]],
-  ['text_5fadd_9',['text_add',['../text_8c.html#a302e1ee5febfa1bc2a547e98beddb78e',1,'text_add(GwText *text, const m_str str):&#160;text.c'],['../gwion__text_8h.html#aeea39a4bc2b29937f77d083967bedd5e',1,'text_add(GwText *, const m_str):&#160;text.c']]],
+  ['text_5fadd_9',['text_add',['../text_8c.html#a12e169fc9d93563391f9946efe86a380',1,'text_add(GwText *text, const char *str):&#160;text.c'],['../gwion__text_8h.html#a9cdc47b67c84e2a87c5908c3074c41a5',1,'text_add(GwText *, const char *):&#160;text.c']]],
   ['text_5fgrow_10',['text_grow',['../text_8c.html#a6676f1e3cf8449a4aa756c029ac9a053',1,'text.c']]],
-  ['text_5frelease_11',['text_release',['../gwion__text_8h.html#a8f885a2b609315784a1ca023e403bd43',1,'gwion_text.h']]],
-  ['text_5freset_12',['text_reset',['../gwion__text_8h.html#a0ee05644261d6721da77d6e82d377153',1,'gwion_text.h']]]
+  ['text_5finit_11',['text_init',['../gwion__text_8h.html#a06a5be3f975a55f3f1a0c96b9a78454c',1,'gwion_text.h']]],
+  ['text_5frelease_12',['text_release',['../gwion__text_8h.html#a8f885a2b609315784a1ca023e403bd43',1,'gwion_text.h']]],
+  ['text_5freset_13',['text_reset',['../gwion__text_8h.html#a0ee05644261d6721da77d6e82d377153',1,'gwion_text.h']]]
 ];
