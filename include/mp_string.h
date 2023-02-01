19c13d54cf5ed8f8e8ce63cca357da3fca28c6ad
fennecdjay <fennecdjay@gmail.com>
2023-02-01 16:15:27 +0100

    :art: small clean

--- include/mp_string.h
+++ include/mp_string.h
@@ -5,12 +5,12 @@
 #define __MP_STRING
 
 /** free mp_allocated string **/
-ANN static inline void free_mstr(MemPool mp, const m_str str) {
+ANN static inline void free_mstr(MemPool mp, char *const str) {
   _mp_free(mp, strlen(str) + 1, str);
 }
 
 /** mp_alloc version of strdup **/
-ANN static inline m_str mstrdup(MemPool mp, const m_str name) {
+ANN static inline m_str mstrdup(MemPool mp, const char *name) {
   const m_str dup = (m_str)_mp_malloc(mp, strlen(name) + 1);
   strcpy(dup, name);
   return dup;
