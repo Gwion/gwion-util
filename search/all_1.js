069874e8c68396483a928d08d85f8dc0f96e038f
fennecdjay <fennecdjay@gmail.com>
2023-02-20 09:14:08 +0000

    Deploying to gh-pages from  @ 89ebe182f7b7e654c8bbf45e675feced02e42c48 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -7,11 +7,12 @@ var searchData=
   ['append_5farg_4',['APPEND_ARG',['../cmdapp_8c.html#a53a887d8da9d71043399f871ae9fb8ca',1,'cmdapp.c']]],
   ['argc_5',['argc',['../structCArg.html#af3b5d5a9540d236de47a57d4937a4f9d',1,'CArg']]],
   ['argsplitter_6',['ArgSplitter',['../structArgSplitter.html',1,'']]],
-  ['argv_7',['argv',['../structCArg.html#aad0d672124af019ef2a43d9be73acf49',1,'CArg']]],
-  ['array_5fcap_8',['ARRAY_CAP',['../m__vector_8h.html#a16c6eace6af476b197d1e2a231440d4b',1,'m_vector.h']]],
-  ['array_5flen_9',['ARRAY_LEN',['../m__vector_8h.html#a44c876c78e2fc722c001dfc1a36b638f',1,'m_vector.h']]],
-  ['array_5foffset_10',['ARRAY_OFFSET',['../m__vector_8h.html#a2b7ea6aed3aa81a25f6b101cda6d735a',1,'m_vector.h']]],
-  ['array_5fptr_11',['ARRAY_PTR',['../m__vector_8h.html#aedaab00e92eb6a8b9797aba4c21f01c0',1,'m_vector.h']]],
-  ['array_5fsize_12',['ARRAY_SIZE',['../m__vector_8h.html#a70c57aae3eb654e205459b4362c8089a',1,'m_vector.h']]],
-  ['author_13',['author',['../structcmdapp__info__t.html#a62bff3f34055a85ce894150de7a6094c',1,'cmdapp_info_t']]]
+  ['argtype_7',['argtype',['../structcmdopt__t.html#af74f27ac98a75b9f4406e04dd10badcf',1,'cmdopt_t']]],
+  ['argv_8',['argv',['../structCArg.html#aad0d672124af019ef2a43d9be73acf49',1,'CArg']]],
+  ['array_5fcap_9',['ARRAY_CAP',['../m__vector_8h.html#a16c6eace6af476b197d1e2a231440d4b',1,'m_vector.h']]],
+  ['array_5flen_10',['ARRAY_LEN',['../m__vector_8h.html#a44c876c78e2fc722c001dfc1a36b638f',1,'m_vector.h']]],
+  ['array_5foffset_11',['ARRAY_OFFSET',['../m__vector_8h.html#a2b7ea6aed3aa81a25f6b101cda6d735a',1,'m_vector.h']]],
+  ['array_5fptr_12',['ARRAY_PTR',['../m__vector_8h.html#aedaab00e92eb6a8b9797aba4c21f01c0',1,'m_vector.h']]],
+  ['array_5fsize_13',['ARRAY_SIZE',['../m__vector_8h.html#a70c57aae3eb654e205459b4362c8089a',1,'m_vector.h']]],
+  ['author_14',['author',['../structcmdapp__info__t.html#a62bff3f34055a85ce894150de7a6094c',1,'cmdapp_info_t']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['argc_0',['argc',['../structCArg.html#af3b5d5a9540d236de47a57d4937a4f9d',1,'CArg']]],
-  ['argv_1',['argv',['../structCArg.html#aad0d672124af019ef2a43d9be73acf49',1,'CArg']]],
-  ['author_2',['author',['../structcmdapp__info__t.html#a62bff3f34055a85ce894150de7a6094c',1,'cmdapp_info_t']]]
+  ['argtype_1',['argtype',['../structcmdopt__t.html#af74f27ac98a75b9f4406e04dd10badcf',1,'cmdopt_t']]],
+  ['argv_2',['argv',['../structCArg.html#aad0d672124af019ef2a43d9be73acf49',1,'CArg']]],
+  ['author_3',['author',['../structcmdapp__info__t.html#a62bff3f34055a85ce894150de7a6094c',1,'cmdapp_info_t']]]
 ];
